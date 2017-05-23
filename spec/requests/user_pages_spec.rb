8c9c946364cbef66a9da97ab4d3be08ef2447be4
Jim Wheaton <jimmy_james@outlook.com>
2017-05-23 08:47:49 -0700

    Finish chp7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,12 +21,20 @@ describe "UserPages" do
   end
 
   describe 'signup page' do
+
     before {visit signup_path}
     let(:submit) { 'Create my account'}
 
     describe 'with invalid information' do
       it 'should not create a user' do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe 'after submission' do
+        before { click_button submit}
+
+        it {should have_selector('title', text: 'Sign up')}
+        it {should have_selector('.alert-error', text: /The form contains \d error(s)?/)}
       end
     end
 
@@ -40,6 +48,14 @@ describe "UserPages" do
 
       it 'should create a user' do
         expect { click_button submit}.to change(User, :count).by(1)
+      end
+
+      describe 'after saving the user' do
+        before { click_button submit}
+        let(:user) { User.find_by_email('joe@horton.com')}
+
+        it { should have_selector('title', text: user.name)}
+        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
       end
     end
   end
