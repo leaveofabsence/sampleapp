1ebc9ab18c4d68fcd1adeae1a7e2ceaf62ca11c5
Jim Wheaton <jimmy_james@outlook.com>
2017-05-29 11:39:21 -0700

    Start microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it {should respond_to(:remember_token)}
   it {should respond_to(:admin)}
   it {should respond_to(:authenticate)}
+  it {should respond_to(:microposts)}
 
   it {should be_valid}
   it {should_not be_admin}
@@ -26,6 +27,31 @@ describe User do
     end
   end
 
+  describe 'micropost associations' do
+    let(:name) do
+      Micropost.find_by_id(m.id).should be_nil
+    end
+
+    before {@user.save}
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it 'should have microposts in the correct order' do
+      @user.microposts.by_date.should == [newer_micropost, older_micropost]
+    end
+
+    it 'should destroy associated microposts' do
+      microposts = @user.microposts
+      @user.destroy
+      microposts.each do |m|
+        Micropost.find_by_id(m.id).should be nil
+      end
+    end
+  end
 
   describe 'with admin attribute set to true' do
     before {@user.toggle!(:admin)}
@@ -129,7 +155,7 @@ describe User do
 
   describe 'remember token' do
     before {@user.save}
-    its(:remember_token) { should_not be_blank}
+    its(:remember_token) {should_not be_blank}
   end
 
 end
